c00c98586a7c56f9c519fa378a8cf702cd0ddf8a
Your Name <you@example.com>
2021-12-23 11:31:29 +0300

    Start project

---
+++ gulp/tasks/html.js
@@ -0,0 +1,50 @@
+import fileinclude from 'gulp-file-include';
+import webpHtmlNosvg from 'gulp-webp-html-nosvg';
+import versionNumber from 'gulp-version-number';
+import pug from 'gulp-pug';
+
+export const html = () => {
+  return (
+    app.gulp
+      .src(app.path.src.html)
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: 'HTML',
+            message: 'error <%= error.message %>',
+          })
+        )
+      )
+      .pipe(fileinclude())
+      /*
+      .pipe(
+        pug({
+          // Сжатие HTML файлов
+          pretty: true,
+          // Показывать в журнале какой файл обработан
+          verbose: true,
+        })
+      )*/
+      .pipe(app.plugins.replace(/@img\//g, 'img/'))
+      .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
+      .pipe(webpHtmlNosvg())
+      .pipe(
+        app.plugins.if(
+          app.isBuild,
+          versionNumber({
+            value: '%DT%',
+            append: {
+              key: '_v',
+              cover: 0,
+              to: ['css', 'js'],
+            },
+            output: {
+              file: 'gulp/version.json',
+            },
+          })
+        )
+      )
+      .pipe(app.gulp.dest(app.path.build.html))
+      .pipe(app.plugins.browsersync.stream())
+  );
+};
